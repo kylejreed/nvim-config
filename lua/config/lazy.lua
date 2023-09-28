89ad1025ad9a0447a74c50c3c6475509a64219c1
Kyle Reed <kylejreed2@gmail.com>
2023-09-28 13:40:00 -0700

    new theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "github_dark", "habamax" } },
+  install = { colorscheme = { "github_dark", "catppuccin-macchiato", "nightfly", "tokyonight" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
